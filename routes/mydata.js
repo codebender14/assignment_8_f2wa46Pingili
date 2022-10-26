15951556a45132269ee8b0eb46d9d2c23683d98d
codebender14 <111784806+codebender14@users.noreply.github.com>
2022-10-26 00:54:23 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var times=0
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  times = times+1
-  res.send('User accesses are: '+times);
+  res.render('mydata', { title: 'Sai Maneendra Pingili' });
 });
 
 module.exports = router;
