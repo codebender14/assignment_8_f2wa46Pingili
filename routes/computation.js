4b3ea59ee45968358ef8fa4b50962b9fb890baa8
codebender14 <111784806+codebender14@users.noreply.github.com>
2022-10-26 13:45:00 -0500

    new endpoint computation

---
+++ routes/computation.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req,res,next)
+{
+    var num1 = Math.floor(Math.random()*10);
+    var num2 = Math.floor(Math.random()*10);
+    var num3 = Math.floor(Math.random()*10);
+    var num4 = Math.floor(Math.random()*10);
+    var atanFunction = Math.atan(num1,num2);
+    var expFunction = Math.exp(num3);
+    var expm1Function = Math.expm1(num4);
+
+    res.render('computation',
+    {
+        title: 'Sai Maneendra Pingili Compuring the functions',
+        num1: num1,
+        num2: num2,
+        num3: num3,
+        num4: num4,
+        atan: atanFunction,
+        exp: expFunction.
+        expm1: expm1Function
+    });
+
+    
+});
+
+module.exports = router;
